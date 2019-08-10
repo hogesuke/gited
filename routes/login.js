4a835042c63de1ec59dd6a09a9d0aa968c403236
hogesuke <miyado@gmail.com>
2019-08-10 14:16:51 +0900

    Fix ESLint errors

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 
-var passport = require('passport');
-var GithubStrategy = require('passport-github').Strategy;
-var LoginConfigration = require('../config/login-configuration');
+var passport = require('passport')
+var GithubStrategy = require('passport-github').Strategy
+var LoginConfigration = require('../config/login-configuration')
 
 /**
  * Github OAuth.
@@ -11,56 +11,56 @@ var githubStrategy = new GithubStrategy({
   clientID: LoginConfigration.Github.clientID,
   clientSecret: LoginConfigration.Github.clientSecret,
   callbackURL: LoginConfigration.Github.callbackURL
-}, login);
-passport.use(githubStrategy);
+}, login)
+passport.use(githubStrategy)
 
 /**
  * OAuth login.
  */
-function login(token, tokenSecret, profile, done) {
+function login (token, tokenSecret, profile, done) {
   var user = {
     raw_name: profile.username,
     token: token,
     type: 'loginUser'
   }
-  console.dir(user);
-  done(null, user);
+  console.dir(user)
+  done(null, user)
 }
 
-passport.serializeUser(function(user, done){
-  done(null, user);
-});
+passport.serializeUser(function (user, done) {
+  done(null, user)
+})
 
-passport.deserializeUser(function(user, done){
-  done(null, user);
-});
+passport.deserializeUser(function (user, done) {
+  done(null, user)
+})
 
 /**
  * ログインチェック。
  */
-exports.checkLogin = function(req, res, next) {
+exports.checkLogin = function (req, res, next) {
   if (req.session.passport.user) {
-    next();
+    next()
   } else {
     var user = {
       raw_name: LoginConfigration.tryUser.raw_name,
       token: LoginConfigration.tryUser.token,
       type: 'tryUser'
-    };
-    req.session.passport.user = user;
-    next();
+    }
+    req.session.passport.user = user
+    next()
   }
-};
+}
 
 /**
  * ログアウトする。
  */
-exports.logout = function(req, res) {
-    req.logout();
-    res.redirect('/');
+exports.logout = function (req, res) {
+  req.logout()
+    res.redirect('/')
 };
 
 /**
  * Strategys.
  */
-exports.githubStrategy = githubStrategy;
+exports.githubStrategy = githubStrategy
