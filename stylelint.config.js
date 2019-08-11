c82648eee8d6ea33b5d5a6fd27b0691b4bbe0c6c
hogesuke <miyado@gmail.com>
2019-08-11 15:20:47 +0900

    Delete comment out lines

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,8 +4,6 @@ module.exports = {
   ],
   extends: 'stylelint-config-standard',
   rules: {
-    // 'no-descending-specificity': null, // FIXME: 検出数が多すぎてすぐには修正できないため一旦offとする
-    // 'font-family-no-missing-generic-family-keyword': null,
     'at-rule-no-unknown': null,
     'scss/at-rule-no-unknown': true,
   }
